76e0041fe8aff8f00dd8cad860ee4206237db5a7
b1anker <narutowontstop@gmail.com>
2018-04-13 23:22:05 +0800

    调整footer

--- src/components/blog/footer/index.jsx
+++ src/components/blog/footer/index.jsx
@@ -1,15 +1,46 @@
 import React, { Component } from 'react'
 import { FooterStyle } from './style'
 
+const BLOG_START_DATE = '2018-04-13 22:57:50'
+const DAY_GAP = 1000 * 60 * 60 * 24
+const HOUR_GAP = 1000 * 60 * 60
+const MINUTE_GAP = 1000 * 60
+const SECOND_GAP = 1000
+
 export default class MyFooter extends Component {
+  constructor (props) {
+    super(props)
+    this.blogStartTimestamp = new Date(BLOG_START_DATE).getTime()
+    this.state = {
+      gap: +new Date - this.blogStartTimestamp
+    }
+  }
+
+  componentDidMount () {
+    setInterval(() => {
+      this.setState({
+        gap: +new Date - this.blogStartTimestamp
+      })
+    }, 1000)
+  }
+
   render () {
     return (
       <FooterStyle>
         <div className="footer-image"></div>
-        <p>© 2018 Hi, B1anker. 抄袭于Diygod. Theme By Sagiri v0.0.16.</p>
-        <p>站点地图. 粤ICP备16024121号.</p>
-        <p>Made with  by B1anker.</p>
+        <p>博客已萌萌哒运行{this.convertGap(this.state.gap)}<span className="face">(●'◡'●)ﾉ♥</span></p>
+        <p>托管于vps. 七牛云 提供 CDN 服务. Google Analytics 提供网站统计服务.</p>
+        <p>© 2018 Hi, B1anker. 抄袭于Diygod. Theme By Sagiri v0.0.16.站点地图. 粤ICP备16024121号.</p>
+        <p>Made with by B1anker.</p>
       </FooterStyle>
     )
+  }
+
+  convertGap (gap) {
+    const day = Math.floor(gap / DAY_GAP)
+    const hour = Math.floor((gap - day * DAY_GAP) / HOUR_GAP)
+    const minute = Math.floor((gap - day * DAY_GAP - hour * HOUR_GAP) / MINUTE_GAP)
+    const second = Math.floor((gap - day * DAY_GAP - hour * HOUR_GAP - minute * MINUTE_GAP) / SECOND_GAP)
+    return `${day}天${hour}小时${minute}分${second}秒`
   }
 }
