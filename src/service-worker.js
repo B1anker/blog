d3920c0502fa352f91c0c082595460fadb68e246
b1anker <narutowontstop@gmail.com>
2019-03-11 00:14:07 +0800

    更新service-worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,59 +1,79 @@
-/* tslint:disable */
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js')
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.1.0/workbox-sw.js')
 
 if (workbox) {
-  const url = new URL(location.href);
-  const debug = url.searchParams.has('debug');
-  const cacheName = '2019-03-10v1'
+  const url = new URL(location.href)
+  const env = url.searchParams.get('env')
+  const cacheVersion = '2019-03-10v2'
+
+  self.addEventListener('install', function (event) {
+    return event.waitUntil(self.skipWaiting())
+  })
+
+  self.addEventListener('activate', async (event) => {
+    const cacheNames = await caches.keys()
+    const cacheNamesToDelete = cacheNames.filter((cacheName) => {
+      return !cacheName.includes(cacheVersion.toString())
+    })
+    await Promise.all(cacheNamesToDelete.map(
+      (cacheName) => caches.delete(cacheName))
+    )
+  })
   workbox.setConfig({
-    debug
+    debug: env === 'development'
   })
   workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
   console.log(`Yay! Workbox is loaded 🎉`)
-  workbox.skipWaiting()
-  workbox.clientsClaim()
-
+  console.log('Workbox: ' + env)
+  workbox.skipWaiting && workbox.skipWaiting()
+  workbox.clientsClaim && workbox.clientsClaim()
   workbox.routing.registerRoute(
-    new RegExp('.*\.html'),
-    workbox.strategies.networkFirst({
-      cacheName
+    new RegExp(`https?://b1anker.com:8080/($|about)`),
+    new workbox.strategies.NetworkFirst({
+      cacheName: 'html/' + cacheVersion
     })
   )
 
   workbox.routing.registerRoute(
-    new RegExp('\/$'),
-    workbox.strategies.networkFirst({
-      cacheName
+    /.*\.css/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'css/' + cacheVersion
     })
   )
-  
+
   workbox.routing.registerRoute(
-    new RegExp('\/static\/.*'),
-    workbox.strategies.cacheFirst({
-      cacheName
+    /.*\.js/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'js/' + cacheVersion
     })
   )
-  
+
   workbox.routing.registerRoute(
-    new RegExp('/api/.*'),
-    workbox.strategies.networkFirst({
-      cacheName
+    /.*\.(?:png|jpg|jpeg|svg|gif)/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'image/' + cacheVersion,
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 20,
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+        })
+      ],
     })
   )
-  
+
   workbox.routing.registerRoute(
-    /cdn\.b1anker\.com\\.*/,
-    workbox.strategies.staleWhileRevalidate({
-      cacheName
+    /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'font/' + cacheVersion,
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 20,
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+        })
+      ],
     })
   )
-  
-  workbox.routing.registerRoute(
-    /(www\.google-analytics\.com|ssl\.google-analytics\.com)/,
-    workbox.strategies.networkFirst({
-      cacheName
-    })
-  )
+
+
 } else {
   console.log(`Boo! Workbox didn't load 😬`)
 }
