8416a7c3c690d0a890f5c673e697021bbc58b339
b1anker <narutowontstop@gmail.com>
2019-09-07 02:00:30 +0800

    fix: 更新upload策略
    全部静态资源上传完才上传index.html

--- build/upload.js
+++ build/upload.js
@@ -3,6 +3,37 @@ const fs = require('fs')
 const path = require('path')
 const axios = require('axios').default
 
+const uploadIndex = () => {
+  axios
+    .put(
+      'http://localhost:3000/api/v1/workflow/update/blog/index',
+      {
+        template: fs.readFileSync(
+          path.resolve(__dirname, '../dist/index.html'),
+          {
+            encoding: 'utf8'
+          }
+        )
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+    .then((res) => {
+      if (res.data.message === 'ok') {
+        console.log('更新index.html成功')
+      } else {
+        console.log(res.data)
+      }
+    })
+    .catch((err) => {
+      console.log('更新index.html失败')
+      console.log(err)
+    })
+}
+
 if (process.env['QINIU_ACCESS_KEY'] && process.env['QINIU_SECRET_KEY']) {
   console.log('开始上传')
   const resolve = (dir) => {
@@ -25,20 +56,28 @@ if (process.env['QINIU_ACCESS_KEY'] && process.env['QINIU_SECRET_KEY']) {
   const formUploader = new qiniu.form_up.FormUploader(config)
   //构造上传函数
   function uploadFile(uploadToken, key, localFile) {
-    formUploader.putFile(uploadToken, key, localFile, putExtra, function(
-      respErr,
-      respBody,
-      respInfo
-    ) {
-      if (respErr) {
-        console.log(respErr)
-        console.log(`${key}上传失败`)
-      }
-      if (respInfo.statusCode == 200) {
-        console.log(respBody)
-      } else {
-        console.log(respInfo.statusCode, respBody)
-      }
+    return new Promise((resolve, reject) => {
+      formUploader.putFile(uploadToken, key, localFile, putExtra, function(
+        respErr,
+        respBody,
+        respInfo
+      ) {
+        if (respErr) {
+          console.log(respErr)
+          console.log(`${key}上传失败`)
+          reject(respErr)
+        }
+        if (respInfo.statusCode == 200) {
+          console.log(respBody)
+          resolve(respBody)
+        } else {
+          console.log(respInfo.statusCode, respBody)
+          reject({
+            statusCode: respInfo.statusCode,
+            respBody
+          })
+        }
+      })
     })
   }
 
@@ -52,30 +91,13 @@ if (process.env['QINIU_ACCESS_KEY'] && process.env['QINIU_SECRET_KEY']) {
     })
   })
 
-  files.forEach((file) => {
-    const fileSplited = file.split('/')
-    const target = fileSplited[fileSplited.length - 1]
-    uploadFile(uploadToken, target, file)
+  Promise.all(
+    files.map((file) => {
+      const fileSplited = file.split('/')
+      const target = fileSplited[fileSplited.length - 1]
+      return uploadFile(uploadToken, target, file)
+    })
+  ).then(() => {
+    uploadIndex()
   })
 }
-
-axios
-  .put(
-    'https://b1anker.com/api/v1/workflow/update/blog/index',
-    {
-      template: fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), {
-        encoding: 'utf8'
-      })
-    },
-    {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }
-  )
-  .then((res) => {
-    console.log(res.data.msg)
-  })
-  .catch((err) => {
-    console.log('更新index.html失败')
-  })
