1c77c9a68109d7ce0579e68aa5a0cb0626cdf894
b1anker <narutowontstop@gmail.com>
2019-09-07 02:03:53 +0800

    fix: 更新上传文件地址

--- build/upload.js
+++ build/upload.js
@@ -6,7 +6,7 @@ const axios = require('axios').default
 const uploadIndex = () => {
   axios
     .put(
-      'http://localhost:3000/api/v1/workflow/update/blog/index',
+      'htts://b1anker.com/api/v1/workflow/update/blog/index',
       {
         template: fs.readFileSync(
           path.resolve(__dirname, '../dist/index.html'),
