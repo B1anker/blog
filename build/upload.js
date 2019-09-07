9ba01bfbcedbfa6d2c3f8fdeb61e12c1c21d54ad
b1anker <narutowontstop@gmail.com>
2019-09-07 10:36:43 +0800

    fix: 优化上传策略
    先看文件是否存在，存在就不重复上传了

--- build/upload.js
+++ build/upload.js
@@ -2,7 +2,9 @@ const qiniu = require('qiniu')
 const fs = require('fs')
 const path = require('path')
 const axios = require('axios').default
+const { Signale } = require('signale')
 
+const signale = new Signale()
 const uploadIndex = () => {
   axios
     .put(
@@ -22,20 +24,66 @@ const uploadIndex = () => {
       }
     )
     .then((res) => {
-      if (res.data.message === 'ok') {
-        console.log('更新index.html成功')
-      } else {
-        console.log(res.data)
-      }
+      signale.success('更新index.html成功')
     })
     .catch((err) => {
-      console.log('更新index.html失败')
-      console.log(err)
+      signale.error('更新index.html失败')
+      signale.error(err)
     })
 }
 
+function statFile({ key, mac }) {
+  const bucketManager = new qiniu.rs.BucketManager(mac, config)
+  return new Promise((resolve, reject) => {
+    bucketManager.stat('cdn-server', key, function(
+      respErr,
+      respBody,
+      respInfo
+    ) {
+      if (respErr) {
+        reject(respErr)
+      } else {
+        if (respInfo.statusCode == 200) {
+          resolve(respBody.fsize)
+        } else {
+          reject({
+            statusCode: respInfo.statusCode,
+            respBody
+          })
+        }
+      }
+    })
+  })
+}
+
+//构造上传函数
+function uploadFile({ uploadToken, key, file, config }) {
+  const putExtra = new qiniu.form_up.PutExtra()
+  const formUploader = new qiniu.form_up.FormUploader(config)
+  return new Promise((resolve, reject) => {
+    formUploader.putFile(uploadToken, key, file, putExtra, function(
+      respErr,
+      respBody,
+      respInfo
+    ) {
+      if (respErr) {
+        reject(respErr)
+      } else {
+        if (respInfo.statusCode == 200) {
+          resolve(respBody)
+        } else {
+          reject({
+            statusCode: respInfo.statusCode,
+            respBody
+          })
+        }
+      }
+    })
+  })
+}
+
 if (process.env['QINIU_ACCESS_KEY'] && process.env['QINIU_SECRET_KEY']) {
-  console.log('开始上传')
+  signale.await('开始上传')
   const resolve = (dir) => {
     return path.resolve(__dirname, '..', dir)
   }
@@ -45,41 +93,12 @@ if (process.env['QINIU_ACCESS_KEY'] && process.env['QINIU_SECRET_KEY']) {
     process.env['QINIU_SECRET_KEY']
   )
 
-  const options = {
-    scope: 'cdn-server'
-  }
   var config = new qiniu.conf.Config()
   config.zone = qiniu.zone.Zone_z2
-  const putPolicy = new qiniu.rs.PutPolicy(options)
+  const putPolicy = new qiniu.rs.PutPolicy({
+    scope: 'cdn-server'
+  })
   const uploadToken = putPolicy.uploadToken(mac)
-  const putExtra = new qiniu.form_up.PutExtra()
-  const formUploader = new qiniu.form_up.FormUploader(config)
-  //构造上传函数
-  function uploadFile(uploadToken, key, localFile) {
-    return new Promise((resolve, reject) => {
-      formUploader.putFile(uploadToken, key, localFile, putExtra, function(
-        respErr,
-        respBody,
-        respInfo
-      ) {
-        if (respErr) {
-          console.log(respErr)
-          console.log(`${key}上传失败`)
-          reject(respErr)
-        }
-        if (respInfo.statusCode == 200) {
-          console.log(respBody)
-          resolve(respBody)
-        } else {
-          console.log(respInfo.statusCode, respBody)
-          reject({
-            statusCode: respInfo.statusCode,
-            respBody
-          })
-        }
-      })
-    })
-  }
 
   const addPrefixToFileName = (prefix, file) => `${prefix}/${file}`
   const dirs = [resolve('dist/static')]
@@ -92,10 +111,36 @@ if (process.env['QINIU_ACCESS_KEY'] && process.env['QINIU_SECRET_KEY']) {
   })
 
   Promise.all(
-    files.map((file) => {
+    files.map(async (file) => {
       const fileSplited = file.split('/')
-      const target = fileSplited[fileSplited.length - 1]
-      return uploadFile(uploadToken, target, file)
+      const key = fileSplited[fileSplited.length - 1]
+      let needUpload = true
+      try {
+        const fsize = await statFile({
+          key,
+          mac
+        })
+        needUpload = fsize <= 0
+      } catch (err) {
+        needUpload = true
+      }
+      if (needUpload) {
+        signale.info(`${key}不存在，需上传`)
+        try {
+          await uploadFile({
+            uploadToken,
+            key,
+            file,
+            config
+          })
+          signale.success(`${key}上传成功`);
+        } catch(err) {
+          signale.error(`${key}上传失败`);
+          signale.error(err)
+        }
+      } else {
+        signale.success(`${key}已存在，跳过上传`);
+      }
     })
   ).then(() => {
     uploadIndex()
